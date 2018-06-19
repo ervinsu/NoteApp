13bc78f6febc1d4b6570f59edeecdbc737d4596f
Ervin Suriandi <ervinsuriandi@yahoo.co.id>
2018-06-19 09:22:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ervin/noteapp/NoteModel.java
@@ -0,0 +1,79 @@
+package com.example.ervin.noteapp;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class NoteModel implements Parcelable {
+    private int id;
+    private String title;
+    private String description;
+    private String date;
+
+
+    public NoteModel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    protected NoteModel(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        description = in.readString();
+        date = in.readString();
+    }
+
+    public static final Creator<NoteModel> CREATOR = new Creator<NoteModel>() {
+        @Override
+        public NoteModel createFromParcel(Parcel in) {
+            return new NoteModel(in);
+        }
+
+        @Override
+        public NoteModel[] newArray(int size) {
+            return new NoteModel[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(title);
+        dest.writeString(description);
+        dest.writeString(date);
+    }
+}
